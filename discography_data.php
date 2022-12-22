99a797c47dea3a94ca0f2659778e4648a6ce168f
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-12-22 14:29:52 +0100

    initial commit

---
+++ discography_data.php
@@ -0,0 +1,67 @@
+<?php
+return [
+  [
+    'Demdouma.jpg',
+    'Demdouma',
+    'Janvier 2018',
+    "1er single de Youcef Shems, \"Demdouma\" est un clip humoristique qui a fait le tour du Maghreb et de l'Europe. Le clip a dépassé le million de vues en quelques semaines.",
+    '-SpXXxlYl24',
+    '3s8YYAoDQ4eDcsO7V2yDuz'
+  ],
+  [
+    'Chaimae.jpg',
+    'Chaimae',
+    'Été 2018',
+    "\"Chaimae\", titre en arabe et en rif qui a été l'un des succès de l'été 2018 au Maroc, on a pu l'entendre dans toutes les Marina, plages et soirées marocaines.",
+    'VvlmjK_tLFQ',
+    '4pILP3u4w8pVs1ptwrWqiN'
+  ],
+  [
+    'Costima.jpg',
+    'Costima',
+    'Janvier 2019',
+    "\"Costima\", un duo avec Yasmine belha dont les vues et streams ont dépassé le million en quelques semaines. Resté trois semaines dans le top 3 des HITS sur une chaîne algérienne (Nessma TV).",
+    'TeDzBYfQuQA',
+    '2SLuwmbaqWDkbYuSuVWWrC'
+  ],
+  [
+    'Mayemi.jpg',
+    'Mayemi',
+    'Été 2019',
+    "\"Mayemi\" (qui veut dire \"pourquoi\" en rif) était le son de l'été 2019 sur la côte rifaine et méditerranéenne du Maroc. Le son était en vente dans plusieurs enseignes et chez plusieurs petits disquaires dans tout le Maroc.",
+    '59HMRM8STOs',
+    '0SZyiiWnuYr7PnfLLHnSvi'
+  ],
+  [
+    'Ladresse.jpg',
+    'L\'adresse',
+    'Fevrier 2020',
+    "Titre en arabe et francais dont le clip a été tourné en noir et blanc, une touche artistique et une composition originale qui ont fait des heureux sur les plateformes musiciales.",
+    'h0X2NuaR3ug',
+    '6ihaGt3QRq6wEyfeDy7nKw'
+  ],
+  [
+    'Hajar.jpeg',
+    'Hajar',
+    'Juillet 2020',
+    "Titre en arabe et en riff qui a été le succès de l’été 2020 au Maroc et plus précisément sur tout le nord du Maroc. Plusieurs reprises ont pu être entendus ici et là dans les soirées marocaines.",
+    'mA2TZ8oeDNw',
+    '4UaSU6ZDGzljcUlXjiWvYX'
+  ],
+  [
+    'VamosCrop.jpg',
+    'Vamos a la Playa',
+    'Juillet 2020',
+    "(<em>partons à la plage</em> en espagnol) titre phare de l'été 2020, un titre en arabe avec un refrain en espagnol qui a enflammé les pistes de toutes les fêtes et boîtes de nuit méditerranéennes.",
+    'Mw3NN6dy1pU',
+    '5jdkBr92rhf74wLffdPbww'
+  ],
+  [
+    'Maroc.jpg',
+    'Allez le Maroc',
+    'Décembre 2022',
+    "Titre spécialement composé pour la coupe du monde 2022 au Qatar. Chanson reprise par les joueurs de l'équipe marocaine de football ainsi que par les supporters marocains.",
+    'jwADDat7KYo',
+    null
+  ]
+];
---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="Youcef Shems: auteur, compositeur et interprète" />
+    <meta name="author" content="amstram.be" />
+    <title>Youcef Shems</title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/custom202212.css" rel="stylesheet" />
+</head>
+<body>
+<header>
+    <h1 class="site-heading text-center text-faded d-none d-md-block">
+        <span class="site-heading-lower cursive">Youcef</span>
+        <span class="site-heading-upper cursive text-primary mb-3">Shems</span>
+    </h1>
+</header>
+    <!-- Navigation-->
+<nav class="navbar navbar-expand-md navbar-dark bg-primary py-lg-4" id="mainNav">
+  <div class="container">
+    <a class="navbar-brand text-secondary cursive fw-bold d-md-none py-2" href="index.php">Youcef Shems</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mx-auto">
+        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase text-secondary" href="#discographie">Discographie</a></li>
+        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase text-secondary" href="#biographie">Biographie</a></li>
+        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase text-secondary" href="#contact">Contact</a></li>
+    </ul>
+    </div>
+  </div>
+</nav>
+
+<section class="page-section clearfix">
+  <div class="container">
+  <div class="intro">
+    <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="assets/img/intro.jpg" alt="..." />
+    <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+        <h2 class="section-heading mb-4">
+            <span class="section-heading-upper">Youcef</span>
+            <span class="section-heading-lower">Shems</span>
+        </h2>
+        <p class="mb-3">Youcef Shems (shems signifie soleil, surnom donné pour sa joie de vivre), est un jeune artiste franco-marocain né dans le Nord de la France. Autodidacte dans l’âme, il est à la fois auteur, compositeur et interprète.</p>
+        <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl rale" href="#biographie">Biographie</a></div>
+    </div>
+  </div>
+  </div>
+</section>
+
+<section class="page-section" id="discographie">
+  <div class="container">
+  <div class="bg-faded rounded  p-4">
+      <h2 class="section-heading mb-4 text-center">
+          <span class="section-heading-upper">Youcef Shems</span>
+          <span class="section-heading-lower">Discographie</span>
+      </h2>
+  <?php
+  foreach (array_reverse(require_once('discography_data.php')) as $single)
+  {
+    ?>
+    <div class="card mb-4">
+      <div class="row">
+        <div class="col-md-4"><img src="assets/img/<?= $single[0];?>" class="card-img-top" alt="Mayemi">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title mb-4 d-flex justify-content-between rale">
+              <strong class="fs-2"><?= $single[1];?></strong>
+              <span><?= $single[2];?></span></h5>
+            <p class="card-text rale"><?= $single[3];?></p>
+            <div class="row">
+              <div class="col-12 col-md-6">
+                <strong class="rale">Voir le clip</strong>
+                <iframe style="border-radius:12px; height:152px; width:100%;"  src="https://www.youtube.com/embed/<?= $single[4];?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+              <div class="col-12 col-md-6">
+                <?php if(isset($single[5])){ ?>
+
+                  <strong class="rale">Ecouter sur Spotify</strong>
+                  <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/<?= $single[5];?>?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                  <?php
+                }?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php
+  }
+  ?>
+  </div>
+  </div>
+</section>
+
+<section class="page-section cta" id="biographie">
+  <div class="container">
+  <div class="cta-inner bg-faded  rounded">
+      <h2 class="section-heading mb-4 text-center">
+          <span class="section-heading-upper">Youcef Shems</span>
+          <span class="section-heading-lower">Biographie</span>
+      </h2>
+      <p>Youcef Shems (<em>shems signifie soleil, surnom donné pour sa joie de vivre</em>), est un jeune artiste franco-marocain né dans le Nord de la France. Autodidacte dans l’âme, il est à la fois auteur, compositeur et interprète.</p>
+      <p>Shems a grandi dans un univers musical poétique bédouin, où <q>les secrets du pouvoir de la musique réside dans l’alliance du verbe et du son</q> <a href="#bio_footnote">*</a>. Son papa passionné par la musique de la région, <em>Beni guil</em> (nomades de l’Est Marocain), chantait lors des évènements locaux accompagné par l’instrument ancestral qu’est la Gasba, flûte en roseau. Il s’inspirait des chanteurs traditionnels tels que cheikh Hamada, Khlefi Ahmed, Cheikh el Madani et bien d’autres abordant de manière subtile des thèmes liés à la condition humaine dans une société traditionnelle. Cette musique s’inspirait des vécus populaires laissant libre court à l’imagination des auditeurs à chercher le sens caché des mots. Cette période a marqué son enfance et Shems puise sa créativité libre de cet héritage poétique et musical.</p>
+      <p>Jeune adulte, il chantait lors des fêtes reprenant les répertoires connus pour ensuite personnaliser les chansons poétiques traditionnelles par des arrangements aux sonorités d’aujourd’hui créant un univers musical particulier.</p>
+      <img class="img-fluid my-3" src="assets/img/biographie.jpg" alt="Photo enfance"/>
+      <p>Son parcours sera marqué par cette période, Shems veut concilier l’héritage que représente sa passion pour l’art du verbe et la modernité faisant partie de son quotidien. Il n’oppose pas ces univers, il les unis à travers ses compositions et ses clips où il met en scène les questions qui marquent la jeunesse.</p>
+      <p>Shems se reconnaît dans le style pop raï, il ouvre son champ musical et se professionnalise en prenant des cours de musique et de chant. Son projet artistique est devenu une passion qui occupe tout son temps libre.  </p>
+      <p>Son premier single « Demdouma » sort en janvier 2018 et aborde de manière humoristique la thématique du mariage arrangé, ce clip dépasse en quelques semaines le million de vues sur les réseaux sociaux.</p>
+      <p>Encouragé par cette belle expérience, il a composé trois duo qui sont de véritables succès en Europe et au Maghreb, chantant en plusieurs langues (français, arabe, néerlandais, rif, ..) il rapproche les différentes communautés des rives de la méditerranée.</p>
+      <p>En prévision de la coupe du Monde 2022, il a sorti sa chanson "Allez le Maroc" pour supporter son pays d’origine. Le Maroc, outsider de la CDM 2022, a excellé et cette chanson a été scandée dans les rues du Maroc et d’Europe.</p>
+      <p class="mt-4">* Des hauts plateaux aux oasis : La chanson saharienne. Abdelhafid Hamdi-Cherif</p>
+  </div>
+  </div>
+</section>
+
+<section class="page-section" id="contact">
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-9 mx-auto">
+          <div class="cta-inner bg-faded text-center rounded py-4">
+              <h2 class="section-heading mb-5">
+                  <span class="section-heading-upper">YOUCEF SHEMS</span>
+                  <span class="section-heading-lower">Contact</span>
+              </h2>
+              <ul class="list-unstyled list-hours mb-5 text-left mx-auto">
+                  <li class="list-unstyled-item list-hours-item">
+                      <!-- alias: 'Y29udGFjdEB5b3VjZWZzaGVtcy5jb20=' -->
+                      <!-- gmail: 'eXNoZW1zdGVhbUBnbWFpbC5jb20=' -->
+                      <a class="ms-auto" href="javascript:decryptEmail('Y29udGFjdEB5b3VjZWZzaGVtcy5jb20=');">
+                        <i class="fa-solid fa-fw fa-envelope"></i>
+                        contact@youcefshems.com
+                      </a>
+                  </li>
+                  <li class="list-unstyled-item list-hours-item ">
+                      <a class="ms-auto" href="tel:+33783491729">
+                        <i class="fa-solid fa-fw fa-phone"></i>
+                        0033 783 491 729
+                      </a>
+                  </li>
+                  <li class="list-unstyled-item list-hours-item">
+                      <a class="ms-auto" href="https://www.instagram.com/youcefshems/">
+                        <i class="fa-brands fa-fw fa-instagram"></i>
+                        youcefshems
+                      </a>
+                  </li>
+                  <li class="list-unstyled-item list-hours-item">
+                      <a class="ms-auto text-secondary" href="https://www.facebook.com/youcefshems/">
+                        <i class="fa-brands fa-fw fa-facebook"></i>
+                        youcef Shems</a>
+                  </li>
+                  <li class="list-unstyled-item list-hours-item">
+                      <a class="ms-auto text-secondary" href="https://www.snapchat.com/add/youcefshems">
+                        <i class="fa-brands fa-fw fa-snapchat"></i>
+                        youceshems</a>
+                  </li>
+                  <li class="list-unstyled-item list-hours-item">
+                      <a class="ms-auto text-secondary" href="https://twitter.com/YoucefShems">
+                        <i class="fa-brands fa-fw fa-twitter"></i>
+                        @YoucefShems</a>
+                  </li>
+                  <li class="list-unstyled-item list-hours-item">
+
+                      <a class="ms-auto" href="https://www.youtube.com/channel/UCQ-KEtzjLPpPdOjUu9XqUrA">
+                        <i class="fa-brands fa-fw fa-youtube"></i>
+                        Ma chaine</a>
+                  </li>
+                  <li class="list-unstyled-item list-hours-item">
+                      <a class="ms-auto" href="https://open.spotify.com/artist/01WhqEB9aeAYvjn5hZs3p0">
+                        <i class="fa-brands fa-fw fa-spotify"></i>
+                        Ma page</a>
+                  </li>
+              </ul>
+
+          </div>
+      </div>
+  </div>
+  </div>
+</section>
+
+<footer class=" text-faded text-center py-5">
+    <div class="container rale">
+      <p class="m-0 small text-primary">Copyright &copy; Youcef Shems <?php echo date('Y');?></p>
+      <p class="m-0 small text-primary">Réalisé par <a href="https://amstram.be">amstram</a></p>
+      </div>
+</footer>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<script type="text/javascript">
+
+  document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 264) {
+          document.querySelector('nav').classList.add('fixed-top');
+          document.querySelector('nav').classList.add('compact');
+
+          navbar_height = document.querySelector('header').offsetHeight / 2;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.querySelector('nav').classList.remove('fixed-top');
+          document.querySelector('nav').classList.remove('compact');
+          document.body.style.paddingTop = '0';
+        }
+    });
+  });
+
+    function decryptEmail(encoded) {
+      var address = atob(encoded);
+      window.location.href = "mailto:" + address;
+
+    }
+</script>
+</body>
+</html>
